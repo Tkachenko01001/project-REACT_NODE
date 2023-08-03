2f84f334ca6233ccaf21172c2c03b231ee1ae71e
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-08-03 23:20:33 +0300

    fixes sprite

--- src/components/MainDashboard/MainDashboard.jsx
+++ src/components/MainDashboard/MainDashboard.jsx
@@ -6,9 +6,9 @@ const MainDashboard = () => {
     return (
       <div className={styles.container}>
         <button className={styles.button}>
-          <svg width={28} height={28} aria-label="plus">
+          <svg width={28} height={28} aria-label="plus" className={styles.svg}>
             <title>Plus Icon</title>
-            <use href={sprite + '#icon-plus1'} />
+            <use href={sprite + '#icon-plus'}/>
           </svg>
           <span className={styles.buttonText}>Add another column</span>
         </button>
