e76eb3d6d0a22ad3673436ef3f993469dc550a20
Lesya-Brodskaya <tubus13@ukr.net>
2023-08-03 09:42:29 +0300

    add component WelcomPage

---
+++ src/components/WelcomePage/WelcomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import css from './WelcomePage.module.css';
+// import sprite from '';
+
+const WelcomePage = () => {
+  return (
+    <container>
+      <section>
+        <h1>Task Pro</h1>
+        <p>
+          Supercharge your productivity and take control of your tasks with Task
+          Pro - Don't wait, start achieving your goals now!
+        </p>
+      </section>
+    </container>
+  );
+};
+
+export default WelcomePage;
