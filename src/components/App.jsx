d11f3c4967e8533f4e83a4c4f2f9e533ed33343c
Ionytch <starik.ionytch@gmail.com>
2023-08-08 02:18:45 +0300

    refresh&logout added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,24 @@ import RestrictedRoute from '../Router/RestrictedRoute';
 import AuthPage from '../pages/AuthPage/AuthPage';
 import WelcomePage from '../pages/WelcomePage/WelcomePage';
 import ScreensPage from './ScreensPage/ScreensPage';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { refreshUser } from 'redux/auth/operations';
+import { selectIsRefreshing } from 'redux/auth/selectors';
+import { Loader } from './Loader/Loader';
 
 const App = () => {
-  return (
+
+  const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <Loader/>
+  ) :(
     <Routes>
       <Route path="/" element={<Navigate to="/welcome" />} />
       <Route path="/welcome" element={<WelcomePage />} />
--- src/components/HeaderDashboard/HeaderDashboard.jsx
+++ src/components/HeaderDashboard/HeaderDashboard.jsx
@@ -1,7 +1,12 @@
 import sprite from '../../images/sprite.svg';
 import styles from './HeaderDashboard.module.css';
 
+import { useDispatch } from 'react-redux';
+import { logOut } from 'redux/auth/operations';
+
 const HeaderDashboard = () => {
+  const dispatch = useDispatch();
+
   return (
     <div className={styles.wrapper}>
       {/* <h2 className={styles.title}>Project office</h2> */}
@@ -12,6 +17,11 @@ const HeaderDashboard = () => {
         </svg>
         <span className={styles.filterText}>Filters</span>
       </button>
+
+      <button className={styles.filter} type="button" onClick={() => dispatch(logOut())}>
+        Logout
+      </button>
+
     </div>
   );
 };
