fbc67f9d18a5e487517afa2145e266cae322a759
Ionytch <starik.ionytch@gmail.com>
2023-08-08 09:39:46 +0300

    logout added to sidebar

--- src/components/HeaderDashboard/HeaderDashboard.jsx
+++ src/components/HeaderDashboard/HeaderDashboard.jsx
@@ -1,12 +1,7 @@
 import sprite from '../../images/sprite.svg';
 import styles from './HeaderDashboard.module.css';
 
-import { useDispatch } from 'react-redux';
-import { logOut } from 'redux/auth/operations';
-
 const HeaderDashboard = () => {
-  const dispatch = useDispatch();
-
   return (
     <div className={styles.wrapper}>
       {/* <h2 className={styles.title}>Project office</h2> */}
@@ -17,11 +12,6 @@ const HeaderDashboard = () => {
         </svg>
         <span className={styles.filterText}>Filters</span>
       </button>
-
-      <button className={styles.filter} type="button" onClick={() => dispatch(logOut())}>
-        Logout
-      </button>
-
     </div>
   );
 };
