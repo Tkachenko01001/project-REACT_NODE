86727f165b74a848f74b9628e4fe1c51cea7d98f
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-08-09 22:44:52 +0300

    adds header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,53 @@
+import css from './Header.module.css';
+
+import { useSelector } from 'react-redux';
+import { selectUser } from 'redux/auth/selectors';
+
+// import { usePopup } from 'hooks/usePopup';
+// import ThemeSelector from 'components/ThemeSelector/ThemeSelector';
+import Avatar from 'components/Avatar/Avatar';
+import Icon from 'components/Icon/Icon';
+
+const Header = () => {
+  const user = useSelector(selectUser);
+
+  // const { getPopup, closePopup } = usePopup();
+
+  return (
+    
+      <div className={css.header}>
+        <div className={css.burgerMenu}>
+          <button className={css.burgerStyle}>
+            <Icon
+              className={css.burgerIcon}
+              name="#icon-menu"
+              width="32px"
+              height="32px"
+            />
+          </button>
+        </div>
+
+        <div className={css.headerSelect}>
+          <div
+            className={css.selectorTheme}
+            // onClick={() => getPopup(<ThemeSelector onClose={closePopup} />)}
+          >
+            <button className={css.styleTheme}>
+              <span>Theme</span>
+              <Icon name="#icon-chevron-down" width="16px" height="16px" />
+            </button>
+          </div>
+
+          <ul className={css.userInfo}>
+            <li className={css.styleName}>{user.name}</li>
+            <li>
+              <Avatar size={32} />
+            </li>
+          </ul>
+        </div>
+      </div>
+    
+  );
+};
+
+export default Header;
