e4b6ed903f376a9b39f586ae5134cc95b583d6c1
Алексей <electrifyx@gmail.com>
2023-08-09 13:20:58 +0300

    styled HomePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,9 +5,9 @@ import ScreensPage from 'components/ScreensPage/ScreensPage';
 export default function HomePage() {
   return (
     <div className={css.home}>
-        <div className={css.headerBox}>Header</div>
       <Layout />
       <div className={css.homeWrap}>
+        <div className={css.headerBox}>Header</div>
         <ScreensPage />
       </div>
     </div>
