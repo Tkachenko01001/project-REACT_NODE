c43b345b5903160021cffe9cde98ae5bb506d19d
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-08-09 20:32:10 +0300

    +

--- src/hooks/usePopup.js
+++ src/hooks/usePopup.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useEffect } from 'react';
+import { createContext, useContext, useState } from 'react';
 
 import Popup from 'components/Popup/Popup';
 
@@ -11,24 +11,6 @@ export const PopupProvider = ({ children }) => {
 
   const [popupContent, setPopupContent] = useState(null);
 
-  // const [lastClick, setLastClick] = useState({});
-
-  // const setClickPosition = e => {
-  //   const { x, y } = e;
-  //   setLastClick({ x, y });
-  // };
-
-  // const getPosition = modalHeight => {
-  //   const { x = 0, y = 0 } = lastClick;
-  //   const pos = {};
-  //   const h = window.innerHeight;
-  //   if (x > window.innerWidth / 2) pos.right = window.innerWidth - x;
-  //   else pos.left = x;
-  //   if (y > h / 2) pos.bottom = h - y + Math.min(0, y - modalHeight);
-  //   else pos.top = y - Math.max(0, modalHeight - (h - y));
-  //   return pos;
-  // };
-
   const getPopup = insert => {
     setPopupContent(insert);
     setShowPopup(true);
@@ -38,10 +20,6 @@ export const PopupProvider = ({ children }) => {
     setPopupContent(null);
     setShowPopup(false);
   };
-  // useEffect(() => {
-  //   // window.addEventListener('click', setClickPosition);
-  //   return () => window.removeEventListener('click', setClickPosition);
-  // }, []);
 
   return (
     <PopupContext.Provider value={{ getPopup, closePopup }}>
