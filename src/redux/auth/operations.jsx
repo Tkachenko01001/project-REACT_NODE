04ee3f72eb8b98378dee3a98373262bdec757f66
OleksandrKambarov <113207904+OleksandrKambarov@users.noreply.github.com>
2023-08-09 16:15:59 +0300

    +

--- src/redux/auth/operations.jsx
+++ src/redux/auth/operations.jsx
@@ -49,20 +49,6 @@ export const logIn = createAsyncThunk(
 );
 
 /*
- * POST @ /users/logout
- * headers: Authorization: Bearer token
- */
-export const logOut = createAsyncThunk('/auth/logout', async (_, thunkAPI) => {
-  try {
-    await axios.post('/api/users/logout');
-    // After a successful logout, remove the token from the HTTP header
-    clearAuthHeader();
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.message);
-  }
-});
-
-/*
  * PATCH @ /user/theme
  * headers: Authorization: Bearer token
  */
@@ -82,6 +68,19 @@ export const changeTheme = createAsyncThunk(
     }
   }
 );
+/*
+ * POST @ /users/logout
+ * headers: Authorization: Bearer token
+ */
+export const logOut = createAsyncThunk('/auth/logout', async (_, thunkAPI) => {
+  try {
+    await axios.post('/api/users/logout');
+    // After a successful logout, remove the token from the HTTP header
+    clearAuthHeader();
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
 
 /*
  * GET @ /users/current
