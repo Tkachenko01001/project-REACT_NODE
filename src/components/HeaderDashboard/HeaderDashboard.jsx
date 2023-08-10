6187261b9033aefcdb4f0cc2f8e09718b3ef1a95
Tkachenko01001 <x380999660502@gmail.com>
2023-08-10 04:44:37 +0300

    fix/filter_modal

--- src/components/HeaderDashboard/HeaderDashboard.jsx
+++ src/components/HeaderDashboard/HeaderDashboard.jsx
@@ -5,19 +5,23 @@ import styles from './HeaderDashboard.module.css';
 
 const HeaderDashboard = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedPriority, setSelectedPriority] = useState('');
 
   const toggleModal = () => setIsOpen(state => !state);
 
-    const options = [
-      { color: 'rgba(255, 255, 255, 0.3)', priority: 'Without priority' },
-      { color: '#8FA1D0', priority: 'Low' },
-      { color: '#E09CB5', priority: 'Medium' },
-      { color: '#BEDBB0', priority: 'High' },
-    ];
+  const options = [
+    { color: 'rgba(255, 255, 255, 0.3)', priority: 'Without priority' },
+    { color: '#8FA1D0', priority: 'Low' },
+    { color: '#E09CB5', priority: 'Medium' },
+    { color: '#BEDBB0', priority: 'High' },
+  ];
+
+  const handlePriorityChange = event => {
+    setSelectedPriority(event.target.value);
+  };
 
   return (
     <div className={styles.wrapper}>
-      {/* <h2 className={styles.title}>Project office</h2> */}
       <button className={styles.filter} onClick={toggleModal}>
         <svg className={styles.icon} width={16} height={16} aria-label="Filter">
           <title>Filter Icon</title>
@@ -38,23 +42,30 @@ const HeaderDashboard = () => {
             </div>
             <div className={styles.modalListOptions}>
               {options.map((option, index) => (
-                <label
-                  key={index}
-                  className={styles.modalCheckbox}
-                >
-                  <span
-                    className={styles.customCheckbox}
-                    style={{ backgroundColor: option.color }}
-                  >
-                    <span className={styles.customBall}></span>
-                  </span>
+                <label key={index} className={styles.radio}>
                   <input
                     type="radio"
                     className={styles.modalListOptionsElement}
                     name="priority"
                     value={`${option.priority}`}
+                    checked={selectedPriority === option.priority}
+                    onChange={handlePriorityChange} 
                   />
-                  <span className={styles.checkboxText}>{option.priority}</span>
+                  <div
+                    className={styles.radioText}
+                    style={{ backgroundColor: option.color }}
+                  ></div>
+                  <div
+                    style={{
+                      color:
+                        option.priority === selectedPriority
+                          ? 'white'
+                          : 'rgba(255, 255, 255, 0.50)',
+                      cursor: 'pointer',
+                    }}
+                  >
+                    {option.priority}
+                  </div>
                 </label>
               ))}
             </div>
