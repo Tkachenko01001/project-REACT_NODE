1754dcc232217fc3a0fbe88d372c179fd4ee4a6a
Tkachenko01001 <x380999660502@gmail.com>
2023-08-11 18:19:12 +0300

    feature/add_columns

--- src/components/MainDashboard/MainDashboard.jsx
+++ src/components/MainDashboard/MainDashboard.jsx
@@ -1,35 +1,58 @@
 import sprite from '../../images/sprite.svg';
 import MainButtonNewBoard from './MainButtonNewBoard/MainButtonNewBoard';
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
+import Column from 'components/Column/Column';
 import styles from './MainDashboard.module.css';
-import { useSelector } from 'react-redux';
-import { selectBoardsList } from 'redux/boards/selectors';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectActiveBoard, selectBoardsList } from 'redux/boards/selectors';
+import AddColumn from 'components/PopUps/AddColumn/AddColumn';
+import Modal from 'components/Modal/Modal';
+import { getActiveBoard } from 'redux/boards/operations';
 
 const MainDashboard = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const toggleModal = () => setIsModalOpen(state => !state);
   const allBoards = useSelector(selectBoardsList);
+  const activeBoard = useSelector(selectActiveBoard);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    allBoards.length > 0 && dispatch(getActiveBoard(allBoards[0]._id))
+  }, [dispatch, allBoards]);
+
+  const columns =
+    activeBoard &&
+    activeBoard.columns &&
+    activeBoard.columns.length > 0 &&
+    activeBoard;
 
   return (
     <>
       {allBoards.length > 0 ? (
         <>
-        <div className={styles.container}>
-          <button className={styles.button}>
-            <svg
-              width={28}
-              height={28}
-              aria-label="plus"
-              className={styles.svg}
-            >
-              <title>Plus Icon</title>
-              <use href={sprite + '#icon-plus'} />
-            </svg>
-            <span className={styles.buttonText}>Add another column</span>
-          </button>
-        </div>
-          </>
+          <div className={styles.container}>
+            <button className={styles.button}>
+              <svg
+                width={28}
+                height={28}
+                aria-label="plus"
+                className={styles.svg}
+                onClick={toggleModal}
+              >
+                <title>Plus Icon</title>
+                <use href={sprite + '#icon-plus'} />
+              </svg>
+              <span className={styles.buttonText}>Add another column</span>
+            </button>
+            {isModalOpen && (
+              <Modal onClose={toggleModal}>
+                <AddColumn toggleModal={toggleModal} />
+              </Modal>
+            )}
+            {columns && <Column />}
+          </div>
+          {}
+        </>
       ) : (
         <div className={styles.mainDashboardContainer}>
           <p className={styles.dashboardDefaultParagraph}>
