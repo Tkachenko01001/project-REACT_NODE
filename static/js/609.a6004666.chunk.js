47a9ed82d2e615bf816e8ce460ca4f187e8fe449
gadgetadd <64710023+gadgetadd@users.noreply.github.com>
2023-08-14 10:52:53 +0000

    Deploying to gh-pages from @ Tkachenko01001/project-REACT_NODE@262e511fe6625267c4f7c45f3ace7e584e9a2ffb 🚀

---
+++ static/js/609.a6004666.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[609],{7609:function(e,a,t){t(2791);a.Z=t.p+"static/media/sprite.892d9162dfcaa580a163c9e6af0b080c.svg"}}]);
+//# sourceMappingURL=609.a6004666.chunk.js.map
